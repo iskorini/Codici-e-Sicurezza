7bc41e6314e80419309a818f8a3d19fb36cc0f56
Federico Schipani <iskorini@gmail.com>
2017-05-09 22:49:09 +0200

    Corretto bug su generazione di numeri primi

--- SecondaEsercitazione/SecondaEsercitazione.py
+++ SecondaEsercitazione/SecondaEsercitazione.py
@@ -1,6 +1,9 @@
 import math
 import random
-import numpy as np
+import timeit
+
+color = ['\033[0m', '\033[31m', '\033[32m', '\033[33m', '\033[34m', '\033[35m', '\033[36m', '\033[37m',
+         '\033[38m', '\033[39m', '\033[40m']
 
 
 def fast_exp_alg(a, n, m):
@@ -11,7 +14,6 @@ def fast_exp_alg(a, n, m):
         if int(i) == 1:
             d = (d * a) % m
     return d
-
 
 def extended_euclidean_algorithm(a, b):
     rm = b
@@ -34,21 +36,22 @@ def extended_euclidean_algorithm(a, b):
 def rabin_test(x, n):
     m, r, xr = n - 1, 0, []
     while m % 2 == 0:
-        m = int(m / 2)
+        m = m // 2
         r = r + 1
     xr.append(fast_exp_alg(x, m, n))
     for i in range(1, r + 1):
         xr.append(fast_exp_alg(xr[i - 1], 2, n))
-    return (xr[0] != 1) & all(xi % n != n - 1 for xi in xr[0:-1])
+    return (xr[0] != 1) and all(xi % n != n - 1 for xi in xr[0:-1])
 
 
 def generate_random_prime(limit, accuracy):
-    random_number = random.randint(1, limit)
-    while random_number % 2 == 0 or any(rabin_test(x, random_number)
-                                        is True for x in range(2, int(random_number / accuracy))):
+    random_number = 0
+    condition = True
+    while condition:
         random_number = random.randint(1, limit)
-        while random_number % 2 == 0:
-            random_number = random.randint(1, limit)
+        if random_number % 2 != 0:
+            test_sample = [random.randint(2, limit) for i in range(0, accuracy)]
+            condition = any(rabin_test(x, random_number) for x in test_sample)
     return random_number
 
 
@@ -93,16 +96,25 @@ def rsa_decrypt_crt(c, km):
     h = km[4] * (m1 - m2) % km[0]
     return m2 + h * km[1]
 
+
+def decryptionexp(n, d, e):
+    return None
+
+
+def wrapper_prime():
+    generate_random_prime(10 ** 100, 16)
+
 if __name__ == '__main__':
     # print(extended_euclidean_algorithm(17, 60))
     # print(rabin_test(2, 457))
     # print(fast_exp_alg(3, 11, 10))
     # print([generate_random_prime(10**100, 4) for i in range(0, 1)])
     # print(rabin_test(2, 3))
-    public_key, private_key = generate_rsa_key(3, 11)
-    print(rsa_encrypt(8, public_key))
-    print(rsa_decrypt(rsa_encrypt(8, public_key), private_key))
-    public_key, private_key = generate_rsa_crt_key(3, 11)
-    encr = rsa_encrypt(8, public_key)
-    print(encr)
-    print(rsa_decrypt_crt(encr, private_key))
+    # public_key, private_key = generate_rsa_key(3, 11)
+    # print(rsa_encrypt(8, public_key))
+    # print(rsa_decrypt(rsa_encrypt(8, public_key), private_key))
+    # public_key, private_key = generate_rsa_crt_key(3, 11)
+    # encr = rsa_encrypt(8, public_key)
+    # print(encr)
+    # print(rsa_decrypt_crt(encr, private_key))
+    print(timeit.timeit(wrapper_prime, number=1))
